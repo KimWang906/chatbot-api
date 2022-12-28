77d8d37a5931e638ff93f09db4c221de4d4454ec
KimWang906 <hyunbin06git@gmail.com>
2022-12-28 16:02:36 +0900

    Update: *

--- app.py
+++ app.py
@@ -3,17 +3,23 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 from flask import Flask, Response, jsonify, request
+import flask_cors
 
 app = Flask(__name__)
+flask_cors.CORS(app)
+
+flask_cors.CORS(app, resources={r'*': {'origins': '*'}})
 
 def cached_model():
     model = SentenceTransformer('jhgan/ko-sroberta-multitask')
     return model
 
+
 def get_dataset():
     df = pd.read_csv('servey_master finish.csv')
     df['embedding'] = df['embedding'].apply(json.loads)
     return df
+
 
 res_generated = []
 res_past = []
@@ -21,13 +27,14 @@ res_past = []
 model = cached_model()
 df = get_dataset()
 
+
 @app.route("/chatbot", methods=["POST"])
 def get_data():
     user_input = request.get_json()
     embedding = model.encode(user_input['buffer'])
 
     df['distance'] = df['embedding'].map(
-    lambda x: cosine_similarity([embedding], [x]).squeeze())
+        lambda x: cosine_similarity([embedding], [x]).squeeze())
     answer = df.loc[df['distance'].idxmax()]
 
     res_past.append(user_input)
@@ -42,7 +49,7 @@ def get_data():
         bot_res = res_generated[i]
 
     response = {
-        "user_qustion": user_input['buffer'],
+        "user_question": user_input['buffer'],
         "bot_answer": bot_res
     }
 
